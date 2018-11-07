703b39484cfd87eb8fdb27a85705f1ab43f4fffc
jimmy_kmt <truongkhoan47kmt@gmail.com>
2018-11-07 09:41:33 +0700

    add entiry framework for code first data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    /// <summary>
+    /// Khởi tạo các đối tượng DBContext, thay vì khởi tạo trực tiếp đối tượng DBContext, ta sẽ khởi tạo thông qua DbFactory.
+    /// </summary>
+    public class DbFactory : Diposable, IDbFactory
+    {
+        private TeduShopDbContext dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext())
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
