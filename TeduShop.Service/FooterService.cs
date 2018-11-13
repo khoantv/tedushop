a149887a61d2e54547676fed939c1d741ef5fca7
jimmy_kmt <truongkhoan47kmt@gmail.com>
2018-11-13 15:04:31 +0700

    add unit test

---
+++ TeduShop.Service/FooterService.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IFooterService
+    {
+        void Add(Footer post);
+
+        void Update(Footer post);
+
+        void Delete(int id);
+
+        Footer GetById(int id);
+
+        IEnumerable<Footer> GetAll();
+
+        IEnumerable<Footer> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        void SaveChanges();
+    }
+
+    public class FooterService : IFooterService
+    {
+        IFooterRepository _footerRepository;
+        IUnitOfWork _unitOfWork;
+
+        public FooterService(IFooterRepository footerRepository, IUnitOfWork unitOfWork)
+        {
+            this._footerRepository = footerRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(Footer footer)
+        {
+            _footerRepository.Add(footer);
+        }
+
+        public void Delete(int id)
+        {
+            _footerRepository.Delete(id);
+        }
+
+        public IEnumerable<Footer> GetAll()
+        {
+            return _footerRepository.GetAll(new string[] { "Footers" });
+        }
+
+        public IEnumerable<Footer> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            return _footerRepository.GetMultiPaging(null, out totalRow, page, pageSize);
+        }
+
+        public Footer GetById(int id)
+        {
+            return _footerRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Footer footer)
+        {
+            _footerRepository.Update(footer);
+        }
+    }
+}
---
+++ TeduShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+
+namespace TeduShop.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private IPostCategoryService _categoryService;
+        private List<PostCategory> listCategory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IPostCategoryRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _categoryService = new PostCategoryService(_mockRepository.Object, _mockUnitOfWork.Object);
+            listCategory = new List<PostCategory>()
+            {
+                new PostCategory(){ID = 1, Name= "DM1", Status = true},
+                new PostCategory(){ID = 2, Name= "DM2", Status = true},
+                new PostCategory(){ID = 3, Name= "DM3", Status = true},
+            };
+
+        }
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            // setup method 
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(listCategory);
+
+            // call action
+            var result = _categoryService.GetAll() as List<PostCategory>;
+
+            //compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
+        [TestMethod]
+        public void PostCategory_Service_Create()
+        {
+            PostCategory postCategory = new PostCategory();
+            postCategory.Name = "Test";
+            postCategory.Alias = "Test";
+            postCategory.Status = true;
+
+            _mockRepository.Setup(m => m.Add(postCategory)).Returns((PostCategory p) =>
+             {
+                 p.ID = 1;
+                 return p;
+             });
+            var result = _categoryService.Add(postCategory);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ID);
+
+        }
+    }
+}
